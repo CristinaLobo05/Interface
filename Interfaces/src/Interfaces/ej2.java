081704c6eb9c818f601b9ddc9bdc7597a0874777
crist <crist@192.168.13.18>
2024-09-30 12:59:44 +0200

    ygu

--- Interfaces/src/Interfaces/ej2.java
+++ Interfaces/src/Interfaces/ej2.java
@@ -5,11 +5,20 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.BoxLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class ej2 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private final JButton btnNewButton = new JButton("Uno");
 
 	/**
 	 * Launch the application.
@@ -37,6 +46,43 @@ public class ej2 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new GridLayout(0, 6, 0, 0));
+		contentPane.add(btnNewButton);
+		
+		JButton btnDos = new JButton("Dos");
+		contentPane.add(btnDos);
+		
+		JButton btnNewButton_1 = new JButton("Tres");
+		contentPane.add(btnNewButton_1);
+		
+		JLabel label_1 = new JLabel("");
+		contentPane.add(label_1);
+		
+		JLabel label_2 = new JLabel("");
+		contentPane.add(label_2);
+		
+		JLabel label = new JLabel("");
+		contentPane.add(label);
+		
+		JButton btnCuatro = new JButton("Cuatro");
+		contentPane.add(btnCuatro);
+		
+		JButton btnCinco = new JButton("Cinco");
+		contentPane.add(btnCinco);
+		
+		JLabel label_3 = new JLabel("");
+		contentPane.add(label_3);
+		
+		JLabel label_4 = new JLabel("");
+		contentPane.add(label_4);
+		
+		JLabel label_5 = new JLabel("");
+		contentPane.add(label_5);
+		
+		JLabel label_6 = new JLabel("");
+		contentPane.add(label_6);
+		
+	
 	}
 
 }
