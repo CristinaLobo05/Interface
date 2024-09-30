2a8d9c049d60e13cc76d43e1fef6379f52c96c52
crist <crist@192.168.13.18>
2024-09-30 12:57:52 +0200

    gfh

--- Interfaces/src/Interfaces/ejercicio2.java
+++ Interfaces/src/Interfaces/ejercicio2.java
@@ -7,13 +7,19 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
 
 public class ejercicio2 extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
 
 	/**
 	 * Launch the application.
@@ -41,24 +47,21 @@ public class ejercicio2 extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Nombre del Alumno: ");
-		lblNewLabel.setBounds(37, 11, 105, 14);
-		contentPane.add(lblNewLabel);
+		btnNewButton_2 = new JButton("Uno");
 		
-		textField = new JTextField();
-		textField.setBounds(139, 8, 96, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		btnNewButton_1 = new JButton("Dos");
 		
-		JLabel lblNewLabel_1 = new JLabel("Nombre de la maquina virtual: ");
-		lblNewLabel_1.setBounds(37, 36, 169, 14);
-		contentPane.add(lblNewLabel_1);
+		btnNewButton = new JButton("Tres");
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(184, 33, 96, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		JButton btnNewButton_2_1 = new JButton("Cuatro");
+		
+		JButton btnNewButton_2_1_1 = new JButton("Cinco");
+		contentPane.setLayout(new GridLayout(0, 5, 0, 0));
+		contentPane.add(btnNewButton_2);
+		contentPane.add(btnNewButton_1);
+		contentPane.add(btnNewButton);
+		contentPane.add(btnNewButton_2_1);
+		contentPane.add(btnNewButton_2_1_1);
 	}
 }
