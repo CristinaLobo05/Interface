6bc3988dc8ab155a800c54613c8b7bcabc411f91
crist <crist@PortatilCris>
2024-10-02 13:30:43 +0200

    fgg

---
+++ Interfaces/src/Interfaces/act4.java
@@ -0,0 +1,16 @@
+package Interfaces;
+
+public class act4 {
+
+	public static void main(String[] args) {
+		
+		int c = 27;
+		
+		int f = (c * 9/5) + 32;
+		
+		System.out.println("27 grados Fahrenheit son " + f + " grados Celsius");
+		
+		
+	}
+	
+}
---
+++ Interfaces/src/Interfaces/act5.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+public class act5 {
+
+	public static void main(String[] args) {
+		
+		int radio = 20;
+		
+		double per = 2 * (3.14 * radio);
+		
+		System.out.println("El perimetro de un circulo con radio 20 es: " + per);
+	}
+	
+}
