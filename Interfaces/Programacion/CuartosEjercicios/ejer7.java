b448688cba793332a1b6a9c0452b38ccdbf50d36
crist <crist@192.168.13.18>
2024-10-18 12:02:03 +0200

    as

--- Interfaces/Programacion/CuartosEjercicios/ejer7.java
+++ Interfaces/Programacion/CuartosEjercicios/ejer7.java
@@ -9,15 +9,12 @@ public class ejer7 {
         int[] numeros = new int[10];
         Random random = new Random();
 
-        // Llenar el array con números aleatorios
+        
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = random.nextInt(100); // Números aleatorios entre 0 y 99
-        }
+            numeros[i] = random.nextInt(100); 
 
-        // Ordenar el array usando Arrays.sort()
-        Arrays.sort(numeros);
+         Arrays.sort(numeros);
 
-        // Imprimir el array ordenado
         System.out.println("Array ordenado: " + Arrays.toString(numeros));
     }
 }
