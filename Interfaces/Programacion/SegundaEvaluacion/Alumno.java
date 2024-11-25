1436657e80bb0bba8bc5fddf95b68474caaa1e42
crist <crist@192.168.13.18>
2024-11-25 09:29:36 +0100

    khj

---
+++ Interfaces/Programacion/SegundaEvaluacion/Alumno.java
@@ -0,0 +1,55 @@
+package SegundaEvaluacion;
+
+public class Alumno {
+
+    // Atributos
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private int edad;
+
+    // Constructor vacío
+    public Alumno() {
+    }
+
+    // Constructor con parámetros
+    public Alumno(String nombre, String apellidos, String dni, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
